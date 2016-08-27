0ac710c95ec5bd8a7ecc35fa7e47f1aca4885055
jonster100 <foxatlarge@outlook.com>
2016-08-27 13:06:51 +0100

    Update AppDriver.java

--- AppDriver.java
+++ AppDriver.java
@@ -11,12 +11,15 @@ public class AppDriver {
 	private int hours;
 	private int noVehicles;
 	private int[][] lookup;
-
-	public AppDriver(int noVeh,int hrs) {
+/**
+ * @param hrs hrs becomes the intitial global int hours
+ */
+	public AppDriver(int hrs) {
 		hours = hrs;
-
 	}
-
+/**
+* Find a  text file and reads the data into the global lookup 2d Array
+*/
 	public void addDataToLookup() {
 		try {
 			String fileName ="lookupdata.txt";
@@ -25,26 +28,31 @@ public class AppDriver {
 				noVehicles = infile.nextInt();
 				lookup = new int[noVehicles][3];
 				for (int i = 0; i < noVehicles; i++){ // reading all the roads in the file
-					int noVeh = infile.nextInt();
-					int minHour = infile.nextInt();
-					int maxHour = infile.nextInt();
+					int noVeh = infile.nextInt(); // the number of verhicles also acting as an id
+					int minHour = infile.nextInt(); // the minimum hour of service for the no of trucks 
+					int maxHour = infile.nextInt(); // the maximum hour of service for the no of trucks 
 					lookup[i][0] = noVeh;
 					lookup[i][1] = minHour;
 					lookup[i][2] = maxHour;
 			}
-				System.out.println("Data Loaded");
+				System.out.println("Data Loaded"); // testing if the data was loaded 
 				infile.close();
 			}
 			catch(FileNotFoundException e) {
-				System.out.println(e.getMessage());
+				System.out.println(e.getMessage()); //  if the files was not found print message
 			}
 	}
 
+/**
+* @param noVeh The parameter is used to find the data in the lookup array acting as an id
+* @param hr The parameter is used to compare the minimum and maximum service required comparing with the dat form the lookup array
+* @return String returns an appropriate response from the entered parameters
+*/
 	private String findData(int noVeh,int hr) {
-		String inOrOut = "";
-		String toBeReturned = "";
+		String inOrOut = ""; // whether the no of hrs parameter is within the standard service time
+		String toBeReturned = ""; // to be returned through the method
 		try {
-				if(hr >= lookup[noVeh-1][1] && hr <= lookup[noVeh-1][2]){
+				if(hr >= lookup[noVeh-1][1] && hr <= lookup[noVeh-1][2]){ // checking the maximum and minumum time of service agaist the lookup array
 					inOrOut = "within";
 				}
 				else {
@@ -52,7 +60,7 @@ public class AppDriver {
 				}
 
 		}
-		catch(ArrayIndexOutOfBoundsException e){
+		catch(ArrayIndexOutOfBoundsException e){ // if the entered noVeh parameter is too little or too much by the size of the lookp array an error message will be returned though the method
 			toBeReturned = "Entered No Vehicles Out of Bounds Limit is 1-51";
 		}
 		if(inOrOut == "within" || inOrOut == "outside"){
@@ -71,23 +79,34 @@ public class AppDriver {
 		return toBeReturned;
 	}
 
-
-
+/**
+* @return Sring a string is reutned thought the method
+*/
 	private String guiOutput(){
 		return "";
 	}
-
-	private void printTestData(){
+	
+/**
+* This method is used to test if the data was loaded correctly into the lookup array
+*/
+	private void printTestData(){ 
 		for(int i = 0; i<noVehicles;i++){
 				System.out.println(lookup[i][0]+":"+lookup[i][1]+":"+lookup[i][2]);
 		}
 	}
 
+/**
+* @return int an int is returned through the method
+*/
 	public int getNoVehicles() {
 		return noVehicles;
 	}
 
-
+/**
+* @param noVeh is passed through to the method
+* @param noHr is passed through to the method
+* @return String is returned though the method
+*/
 	public String run(int noVeh,int noHr){
 		//printTestData();
 		return findData(noVeh,noHr);
